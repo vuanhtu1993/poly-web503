3db4baf4e095b8b7f31cc7e713b97cd7c341a880
tuva9 <tuva9@fpt.edu.vn>
2023-06-26 11:15:20 +0700

    Buổi 1: HTTP module

---
+++ lesson1/server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer(function (req, res) {
+    console.log(req);
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    })
+    res.write("Hello world")
+    res.end()
+})
+
+server.listen(8080)
