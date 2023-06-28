a12d606bd7fe57ccd81402f0ca82a8ec36292466
tuva9 <tuva9@fpt.edu.vn>
2023-06-28 09:57:10 +0700

    Buổi 2: Install nodemon

--- lesson1/server.js
+++ lesson1/server.js
@@ -1,11 +1,15 @@
 const http = require('http')
 
+const data = [
+
+]
+
 const server = http.createServer(function (req, res) {
     console.log(req);
     res.writeHead(200, {
         "Content-Type": "text/html"
     })
-    res.write("Hello world")
+    res.write("Xin chao ca lop update")
     res.end()
 })
 
