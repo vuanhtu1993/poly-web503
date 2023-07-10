5f186bd128f454aeb1666a430b9039dbbfde7540
tuva9 <tuva9@fpt.edu.vn>
2023-07-10 10:23:02 +0700

    Buổi 4: Refactor cont

--- lesson2/src/server.js
+++ lesson2/src/server.js
@@ -2,6 +2,11 @@ import express from 'express'
 import fs from 'fs'
 import bodyParser from 'body-parser'
 import movieRouter from './routers/movies'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
 
 const app = express()
 const port = 8080
@@ -15,10 +20,12 @@ app.use(bodyParser.json())
 
 // Routing
 app.get('/', function (req, res) {
-    const html = fs.readFileSync('./pages/home.html', 'utf-8')
-    res.send(html)
-    res.end()
+    // const html = fs.readFileSync(join(__dirname, '/pages/home.html'), 'utf-8')
+    // res.send(html)
+    res.end(__dirname)
 })
+// Static
+app.use(express.static('src/public'))
 
 // Middleware
 app.use("/movies", movieRouter)
